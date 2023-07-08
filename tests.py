af69a99ff53005c095e1105fa3459efd3bbe4171
enjel091801 <angelicatubac0918@gmail.com>
2023-07-08 18:28:01 +0800

    add test case for update functionality

--- tests.py
+++ tests.py
@@ -84,5 +84,36 @@ class NoteAppTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Test Note', response.data)
 
+    def test_update_note(self):
+        #Log in the user
+        response = self.client.post('/',data={
+        'username': os.getenv("TEST_USER"),
+        'password': os.getenv("TEST_PASSWORD")
+        }, follow_redirects = True)
+        self.assertEqual(response.status_code, 200)
+
+        #Create a note
+        response = self.client.post('/notes',data={
+        'title': 'Test Note',
+        'content': 'This is a test note'
+        }, follow_redirects = True)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(Note.query.all()),1)
+        note = Note.query.first()
+        self.assertEqual(note.title, 'Test Note')
+        self.assertEqual(note.content, 'This is a test note')
+        self.assertEqual(note.user_id, self.user_id)
+
+        #Update Note
+        response = self.client.post(f'/notes/{note.id}', data ={
+            'title': "Updated Note",
+            'content': "Updated Content"
+        }, follow_redirects = True)
+
+        self.assertEqual(response.status_code,200)
+        updated_note = Note.query.get(note.id)
+        self.assertEqual(updated_note.title, "Updated Note")
+        self.assertEqual(updated_note.content, "Updated Content")
+        
 if __name__ == '__main__':
     unittest.main()
